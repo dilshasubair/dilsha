206673f166940410cef71b138c9e0116abd8491f
veena <vk9646481@gmail.com>
2022-11-15 11:59:38 +0530

    my second prgrm

---
+++ DIS.py
@@ -0,0 +1,14 @@
+tthisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+for x in thisdict:
+  print(thisdict[x])
+for x, y in thisdict.items():
+  print(x, y)
+for x in thisdict.values():
+   print(x)
+   
